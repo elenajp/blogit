4857823ce545796632cef697dd71e2886cc74f32
elena perez <ejperezk@gmail.com>
2021-01-26 12:30:04 +0100

    added html pages and relocated templates folder

--- blogit/blogit/settings.py
+++ blogit/blogit/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'blogit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- blogit/blogit/urls.py
+++ blogit/blogit/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
-    path('', views.home)
+    path('', views.home),
 ]

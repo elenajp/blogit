4f02c07cd05c1fd9edd77c05d570e327d46e8f32
elena perez <ejperezk@gmail.com>
2021-05-06 17:19:30 +0200

    did a massive clean up of my project

--- blogit/settings.py
+++ blogit/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = env.str('SECRET_KEY', 'sample_unsafe_secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

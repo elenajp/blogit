341502fd0538edf61b51baec94160682045d39aa
elena perez <ejperezk@gmail.com>
2021-05-09 09:15:41 +0200

    added url parameters so when the slug is entered in the url it will direct the user to the correct page

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Article
@@ -8,3 +9,7 @@ from .models import Article
 def article_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request, 'articles/article_list.html', {'articles': articles})
+
+
+def article_detail(request, slug):
+    return HttpResponse(slug)

9f40b44e9b4f14fd5b8c913e17dea9253a0079d4
elena perez <ejperezk@gmail.com>
2021-05-09 10:25:52 +0200

    added named urls so when the name is clicked it directs to the blog title url. also added the pen image as a logo but I will change it to something better later

--- articles/urls.py
+++ articles/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    path('', views.article_list),
-    path('<slug:slug>/', views.article_detail)
+    path('', views.article_list, name="list"),
+    path('<slug:slug>/', views.article_detail, name="detail")
 ]

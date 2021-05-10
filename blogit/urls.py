8651b7f2f3c57443887ace693034b8cb55e4673c
elena perez <ejperezk@gmail.com>
2021-05-10 12:32:57 +0200

    added urlpatterns to urls.py so django knows where to find media files

--- blogit/urls.py
+++ blogit/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
@@ -13,3 +15,4 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

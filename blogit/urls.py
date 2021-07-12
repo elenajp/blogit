fbb782708146fc2950ae5fcda79bc78744d66a53
elena perez <ejperezk@gmail.com>
2021-07-12 13:46:31 +0200

    got user signup form working

--- blogit/urls.py
+++ blogit/urls.py
@@ -9,8 +9,8 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
     path('articles/', include('articles.urls')),
+    path('accounts/', include('accounts.urls')),
     path('about/', views.about),
     path('', views.homepage),
 ]
